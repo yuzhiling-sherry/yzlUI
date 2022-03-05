c8d41ec5737001ec13e3a668103764c6b1010a05
Zhiling-yu <260512777@qq.com>
2022-03-05 12:10:58 +0800

    vuepress-config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  title:'Yzl-UI',
+  title: 'Yzl-UI',
   base: '/yzlUI/',
   themeConfig: {
-    nav:[{
-      text:'Home',link:'/'
-    },{
-      text:'Github',link:'https://yuzhiling-sherry.github.io/yzlUI/'
-    },{
-      text:'Npm',link:'https://www.npmjs.com/package/yzl-ui'
+    nav: [{
+      text: 'Home', link: '/'
+    }, {
+      text: 'Github', link: 'https://github.com/yuzhiling-sherry/yzlUI'
+    }, {
+      text: 'Npm', link: 'https://www.npmjs.com/package/yzl-ui'
     }],
     sidebar: [
       '/',
